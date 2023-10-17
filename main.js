e64284165faa3c5182bd7d0782b8bfa8a3cbee6d
colaneri <marina_colaneri@hotmail.com>
2023-10-17 15:51:52 -0300

    tercera-entrega

--- main.js
+++ main.js
@@ -1,83 +1,37 @@
-let nombre = prompt ("¿cual es su nombre?");
-nombre = nombre.toUpperCase();
+const saludoBtn = document.getElementById("saludo");
+saludoBtn.addEventListener("click", function() {
+  const nombreUsuario = document.getElementById("nombre").value;
+  alert(`¡Hola, ${nombreUsuario}! Bienvenido a MAMILAS Ropa informal`);
+});
 
-alert ("bienvenido a MAMILAS Ropa informal" + " " + nombre); 
-
-let prendas =  {
-    buzo: 5200,
-    remera: 3200,
-    jean: 9000, 
-}
-console.log(prendas); 
-
-let talles = [ "small" , "madium" , "large" , "36" , "38" , "40" , "42" , "44" , "46"];
-talles.push ("Xsmall");
-console.log (talles)
-
-
-let consulta = prompt("Ingrese la prenda que desea comprar: \n1 Remera \n2 Jean \n3 Buzo");
-
-
-if (consulta == 1){
-
-    
-    alert ('Hay stock en talle \n1 small \n2 large'); 
-    
-    let talle = prompt ("¿que talle buscas?"); 
-
-    if (talle == 1) {
-
-        alert ('Hay stock')
-        
-
-    }else if(talle == 2){
-
-        alert ('Hay stock')
-      
-    }
-}else if (consulta == 2){
-
-    let jean = prompt ("ingrese el talle");
-
-    if (jean >= 36 && jean < 46) {
-
-    alert ('Hay stock!');
-
-    }else if (jean < 36 && jean  >= 46) {
-    
-        alert ('No tenemos stock disponible');
-    }
-   
-}else if (consulta == 3){
-      
-        alert ('Hay stock en talle \n1 small \n2 medium');
-
-        let talle = prompt ("¿que talle buscas?"); 
-    
-    if (talle == 1){
-
-        alert ('Hay stock')
-      
-    }else if (talle == 2){
-
-        alert ('Hay stock')
- 
-     }else{
- 
-        alert ('No hay stock')
-     }
-
-} 
- 
-let buzo = 5200
-let remera = 3200
-let jean = 9000
-
-function sumarCompra (buzo, remera, jean ){
-    return buzo + remera + jean;
-  
+function tallesDisponibles(nombre, precio) {
+  const selectId = `talles${nombre}`;
+  const selectElement = document.getElementById(selectId);
+  selectElement.style.display = "block";
 }
 
-    console.log(sumarCompra (5200,3200,9000));
+function carritoCompras(nombre, precio, selectId) {
+  const selectElement = document.getElementById(selectId);
+  const talleSeleccionado = selectElement.value;
 
- 
+  if (talleSeleccionado) {
+    const nuevoProducto = document.createElement("li");
+    nuevoProducto.textContent = `${nombre} - Talle: ${talleSeleccionado} - Precio: $${precio}`;
+
+    const carrito = document.getElementById("carrito");
+    carrito.appendChild(nuevoProducto);
+
+    const total = document.getElementById("total");
+    const precioss = parseFloat(precio); 
+    const totalCompra = parseFloat(total.textContent);
+    total.textContent = (totalCompra + precioss).toFixed(2); 
+  }
+}
+
+const finalizarCompraBtn = document.getElementById("finalizar");
+finalizarCompraBtn.addEventListener("click", function() {
+  const nombreUsuario = document.getElementById("nombre").value;
+
+  const total = document.getElementById("total").textContent;
+  alert(`¡Gracias por tu compra! Total: $${total}`);
+});
